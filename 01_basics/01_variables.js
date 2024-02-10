d3b809c1ca3fcf126836ba2cacc6b59bc0fdc1ec
Sudip-12 <110447880+Sudip-12@users.noreply.github.com>
2024-02-10 10:45:18 +0000

    Config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345 
+let accountEmail = "hscbhsb@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+ 
+accountEmail = "vcgc@bc.com"
+accountPassword = " 221540"
+accountCity = "Mumbai"
+let accountState;   // undefined
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ; // treat all js code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+// console.log(3
+//     + 3);  // code readability should be high
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value assigned at the moment
+// symbol = unique
+
+// object
+
+
+
+
+
