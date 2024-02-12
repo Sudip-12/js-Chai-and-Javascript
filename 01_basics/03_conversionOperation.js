bc1e7a6ef2a59ac24db26e14180677c29fabaca8
Sudip-12 <110447880+Sudip-12@users.noreply.github.com>
2024-02-12 19:42:34 +0000

    Conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,7 +26,49 @@
 // "" => false
 // "Sudip" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************* Operations ***********
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Hi"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2" + 2);
+
+// console.log(+true);
+// console.log(+"");
+// console.log(+false);
+
+// let num1, num2, num3
+// num1=num2=num3=2+2
+// console.table([num1,num2,num3])
+
+// let gameCounter = 100
+// // gameCounter++;
+// // console.log(gameCounter);
+// console.log(gameCounter++);
+// console.log(++gameCounter);
+
+
+
