9bb68a5f508854b5d6ed56a84a3f11556f9b7407
Sudip-12 <110447880+Sudip-12@users.noreply.github.com>
2024-02-12 19:05:39 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+
+// const {score} = req.body
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+// "33" => 33
+// "33abc" => NaN (NotaNumber)
+// true => 1; False => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Sudip" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
