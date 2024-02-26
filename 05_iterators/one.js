206a8f65c6594b045b8e60ad3d8e91fc45250a16
Sudip-12 <110447880+Sudip-12@users.noreply.github.com>
2024-02-26 19:52:41 +0000

    Loops

---
+++ 05_iterators/one.js
@@ -0,0 +1,63 @@
+// for
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(i);
+//     if (i == 5) {
+//     console.log("5 is best number");        
+//     }
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop : ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner loop : ${j}`);
+//     };
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//         console.log(`Outer loop : ${i}`);
+//         for (let j = 0; j <= 10; j++) {
+//             // console.log(`Inner loop : ${j}`);
+//             console.log(i + "*" + j + "=" + i*j);
+//         };
+//     }
+
+
+// let myArray = ["flash", "batman" , "superman"]
+// console.log(`Length of the array is : ${myArray.length}`);
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i === 5) {
+//         console.log("Detected 5");
+//         break;
+//     }
+//     console.log(`Value of i is : ${i}`);
+    
+// }
+for (let i = 0; i <= 20; i++) {
+    if (i === 5) {
+        console.log("Detected 5");
+        continue
+    }
+    console.log(`Value of i is : ${i}`);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05_iterators/two.js
@@ -0,0 +1,29 @@
+// let i = 0
+// while (i <= 10) {
+//     console.log(`Value of i is : ${i}`);
+//     i = i+2
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is : ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// do while
+
+// let score = 1
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
+
+
