ce143d4e99dc18929b250e5161c546f65e5b55b7
Sudip-12 <110447880+Sudip-12@users.noreply.github.com>
2024-02-26 05:39:47 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,19 +8,55 @@
 
 // scope {}
 // var c = 300
-let a = 300;
-if (true) {
-    let a = 10
-    const b = 20
-    // var c = 30
-    console.log(a);
-}
-console.log(a);
+// let a = 300;
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     // var c = 30
+//     console.log(a);
+// }
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+// Nested scope
+
+function one(){
+    const username= "Sudip"
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+    const username = "Sudip"
+    if (username === "Sudip") {
+        const website = "Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
 
 
+// ++++++++++++++++ Interesting +++++++++++++++
+// isme hum func def k pehele bhi call kr skte hai kyuki ye ek proper function declaration hai
+addone(5)
+function addone(num){
+    return num + 1
+}
+
+// addtwo(5)
+// is way me hum function ko declaration se pehele call nhi kr skte kyuki usse hum ek variable me store kr rhe hai also knwn as hoisting
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
 
 
 
